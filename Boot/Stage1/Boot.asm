573deff3f5bc6f5c9f9d0adb51dd05c40c293a66
Averi <averyb.personal@gmail.com>
2025-12-27 18:42:37 -0500

    Boot System v1.1

--- Boot/Stage1/Boot.asm
+++ Boot/Stage1/Boot.asm
@@ -1,6 +1,11 @@
 BITS 16
 ORG 0x7c00 ; Boot sector load address
 
+jmp short MyaS1_start
+
+Mya_Boot_Drive: db 0
+Mya_Boot_Success_MSG: db "Mya S1: OK", 0
+
 MyaS1_start:
     jmp 0x0000:.MyaS1_main
     .MyaS1_main:
@@ -15,18 +20,23 @@ MyaS1_start:
         mov [Mya_Boot_Drive], dl
 
         mov ax, (MyaS2_start - MyaS1_start) / 512
-        mov cx, (MyaKernel_end - MyaS2_start) / 512
+        mov cx, 1
 
         mov bx, MyaS2_start
         xor dx, dx
 
         call MyaR_Read_Disk
 
+        mov si, Mya_Boot_Success_MSG
+        call MyaR_PrintLine
+
         jmp MyaS2_start
 
-Mya_Boot_Drive: db 0
+Mya_Hang: hlt
+    jmp Mya_Hang
 
-%include "Disk.asm"
+%include "Boot/Stage1/Print.asm"
+%include "Boot/Stage1/Disk.asm"
 
 times 510 - ($ - $$) db 0
 dw 0xAA55 ; Boot signature
