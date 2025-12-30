20db456a4d13bf5f4a7c870053cd54cdbf25d4a9
Averi <averyb.personal@gmail.com>
2025-12-30 16:06:20 -0500

    Print System v1.1

--- Boot/Stage1/Print.asm
+++ Boot/Stage1/Print.asm
@@ -1,13 +1,11 @@
 BITS 16
 
-Newline dw 2
-
-db 13
-db 10
-
 MyaR_Print:
     push ax
+    push bx
     push si
+
+    mov bh, 0x00
 
 .Next:
     lodsb
@@ -16,14 +14,13 @@ MyaR_Print:
     jz .Done
 
     mov ah, 0x0E
-    mov bh, 0x00
-
     int 0x10
 
     jmp .Next
 
 .Done:
     pop si
+    pop bx
     pop ax
 
     ret
@@ -31,13 +28,13 @@ MyaR_Print:
 MyaR_PrintLine:
     call MyaR_Print
 
-    mov al, 13
     mov ah, 0x0E
+    mov bh, 0x00
 
+    mov al, 13
     int 0x10
 
     mov al, 10
-    
     int 0x10
     
     ret
