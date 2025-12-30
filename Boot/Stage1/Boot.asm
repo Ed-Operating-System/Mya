56ce715d4b9294035d96373b128f26c2a87e76a8
Averi <averyb.personal@gmail.com>
2025-12-30 16:05:19 -0500

    Boot System v1.4

--- Boot/Stage1/Boot.asm
+++ Boot/Stage1/Boot.asm
@@ -7,15 +7,9 @@ Mya_Boot_Drive: db 0
 Mya_Boot_Start_MSG: db "Entering Mya Stage 1...", 0
 Mya_Boot_Success_MSG: db "Mya S1: OK", 0
 
-MYA_VERSION_MSG: db "Mya Bootloader Version 1.2", 0
+MYA_VERSION_MSG: db "Mya Bootloader Version 1.3", 0
 MYA_VERSION_DIVIDER_MSG: db "--------------------------", 0
 MYA_BLANK_LINE_MSG: db "", 0
-
-MyaS2_SECTORS equ 4
-MyaS2_LBA equ 1
-
-MyaS2_SEGMENT equ 0x1000
-MyaS2_OFFSET equ 0x0000
 
 MyaS1_start:
     call .MyaS1_main
@@ -34,21 +28,31 @@ MyaS1_start:
     mov si, Mya_Boot_Start_MSG
     call MyaR_PrintLine
 
+    cli
+
     xor ax, ax ; Zero all segment registers
     mov ss, ax
     mov ds, ax
     mov es, ax
 
-    mov sp, 0x7c00 ; Equiv to MyaS1_start   
+    mov sp, 0x7c00 ; Equiv to MyaS1_start
+
+    sti
     cld
     
     mov [Mya_Boot_Drive], dl
-    ;call MyaR_Read_Disk
+
+    mov ax, (Mya_Stage2_start - Mya_Stage1_start) / 512
+    mov cx, (Mya_Stage2_end - Mya_Stage2_start) / 512
+    mov bx, 0x7e00
+    xor dx, dx
+
+    call MyaR_Read_Disk
     
     mov si, Mya_Boot_Success_MSG
     call MyaR_PrintLine
 
-    call MyaS2_start
+    jmp 0x0000:0x7E00 ; Equiv to call MyaS2_start
 
 Mya_Hang: hlt
     jmp Mya_Hang
